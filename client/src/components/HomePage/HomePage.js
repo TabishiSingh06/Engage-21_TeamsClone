b2e4a0f5ccfd0647d210d302ab641332d52bed54
tabishi06 <69653249+tabishi06@users.noreply.github.com>
2021-07-12 12:38:38 +0530

    final commit

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -14,7 +14,7 @@ const HomePage = () => {
                 <div className="leftBody"> <h2> Teams Clone </h2>
                     <p>Get in touch with </p>
                     <p>your friends and family!</p>
-                    <a href="https://drive.google.com/file/d/1nO7LkAs33TbhIcanOUy7enVcfGO7bYtd/view?usp=sharing">This is the video link for the project</a>
+                    <a href="https://youtu.be/_2-Nkws7BiE">This is the video link for the project</a>
                 </div>
                 <div className="rightBody">
                     <img src={body_vector} alt="video_call_people" />
